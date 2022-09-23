fc42436aa0eda7415ad14c7b4d333a6513752c3a
Easton Smith <eastonsmith37@gmail.com>
2022-09-23 14:14:22 -0400

    project build and structure done. Can add users to firebase database from create account page.

--- app/src/main/java/com/mealer/ui/SignUpPage.java
+++ app/src/main/java/com/mealer/ui/SignUpPage.java
@@ -3,12 +3,36 @@ package com.mealer.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.mealer.app.*;
 
 public class SignUpPage extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    EditText confirmPassword;
+
+    Button createAccount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+
+        email = findViewById(R.id.createAccountEmail);
+        password = findViewById(R.id.createAccountPassword);
+        confirmPassword = findViewById(R.id.createAccountPassword2);
+
+        createAccount = findViewById(R.id.signupButton);
+        createAccount.setOnClickListener(v -> {
+            String passwordText = password.getText().toString();
+            if(passwordText.equals(confirmPassword.getText().toString())) {
+                User signedIn = new CookUser("first", "last", email.getText().toString(), "address",
+                        passwordText, "description");
+            }
+        });
     }
 }

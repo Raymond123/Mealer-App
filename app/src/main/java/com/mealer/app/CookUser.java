6da277298dfc5eab33d910aa237fd50fcdc3e595
Easton Smith <eastonsmith37@gmail.com>
2022-09-29 15:01:01 -0400

    Changed main class names, MainActivity.java now is for "get started page", SignUpPage.java is still sign up page, and LoginPage.java now is for current users to login.

--- app/src/main/java/com/mealer/app/CookUser.java
+++ app/src/main/java/com/mealer/app/CookUser.java
@@ -2,19 +2,41 @@ package com.mealer.app;
 
 import android.provider.ContactsContract;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class CookUser extends User{
 
     private String description;
     // picture???
 
-    public CookUser(String firstName, String lastName, String email, String address, String password, String description) {
-        super(firstName, lastName, email, address, password);
+    public CookUser(String firstName, String lastName, String email, String phoneNumber, String address, String password, String description) {
+        super(firstName, lastName, email, phoneNumber, address, password);
         this.description = description;
 
         DatabaseReference databaseReference = getReference("users/cook");
         databaseReference.child(getId()).setValue(this);
+
+        /*
+        databaseReference.child(getId()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    generateNewId();
+                }
+                databaseReference.child(getId()).setValue(this);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        */
 
     }
 

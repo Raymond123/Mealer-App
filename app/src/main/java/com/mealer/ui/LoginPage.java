ada16b07516287fa64e91412fd9abd9380d61006
Nizar Zariouh <nizarzariouh05@gmail.com>
2022-10-03 11:26:36 -0400

    Already exists account text now is clickable and redirect to the login page

--- app/src/main/java/com/mealer/ui/LoginPage.java
+++ app/src/main/java/com/mealer/ui/LoginPage.java
@@ -3,14 +3,16 @@ package com.mealer.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 // LOGIN PAGE
-public class LoginPage extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity{
 
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
+
 }
--- app/src/main/java/com/mealer/ui/MainActivity.java
+++ app/src/main/java/com/mealer/ui/MainActivity.java
@@ -4,20 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 // GET STARTED
 public class MainActivity extends AppCompatActivity {
 
-    Button signIn;
-
+    Button signUp;
+    TextView accountExists;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started_page);
 
-        signIn = findViewById(R.id.getStartedButton);
-        signIn.setOnClickListener(v -> {
+
+        accountExists = findViewById(R.id.accountExists);
+        accountExists.setOnClickListener(view -> {
+                startActivity(new Intent(MainActivity.this, LoginPage.class));
+        });
+        signUp = findViewById(R.id.getStartedButton);
+        signUp.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, SignUpPage.class));
         });
     }

cfae08a6937a343e75e0772ceedd510181e58012
Easton Smith <eastonsmith37@gmail.com>
2022-11-29 12:53:26 -0500

    fixed casting issue on AccountPageFragment.java, work for cooks viewing account page. Need to merge before testing client side.

--- app/src/main/java/com/mealer/ui/ui/account/AccountPageFragment.java
+++ app/src/main/java/com/mealer/ui/ui/account/AccountPageFragment.java
@@ -9,14 +9,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mealer.app.Admin;
 import com.mealer.app.ClientUser;
 import com.mealer.app.CookUser;
@@ -24,7 +29,11 @@ import com.mealer.app.User;
 import com.mealer.ui.LoginPage;
 import com.mealer.ui.databinding.FragmentAccountPageBinding;
 
+import java.util.Objects;
+
 public class AccountPageFragment extends Fragment {
+
+    private static final String TAG = "AccountPageFragment";
 
     // initializing activity elements
     private TextView userType;
@@ -35,6 +44,7 @@ public class AccountPageFragment extends Fragment {
     private Admin signedIn;
 
     FragmentAccountPageBinding binding;
+    DatabaseReference dbRef;
     FirebaseAuth mAuth;
 
     @SuppressLint("SetTextI18n")
@@ -47,7 +57,7 @@ public class AccountPageFragment extends Fragment {
 
         mAuth = FirebaseAuth.getInstance();
 
-        Intent intent = this.getActivity().getIntent();
+        Intent intent = this.requireActivity().getIntent();
         userType = binding.userType;
         userName = binding.userName;
         userDescription = binding.userCookDescription;
@@ -61,30 +71,59 @@ public class AccountPageFragment extends Fragment {
 
         // get current firebase user from firebase authentication
         FirebaseUser currentFirebaseUser = mAuth.getCurrentUser();
+        dbRef = FirebaseDatabase.getInstance().getReference("users");
         // display user type on home page
-        if(this.signedIn.getClass() != Admin.class){
-            User currentUser = (User) this.signedIn;
+        if(this.signedIn.getClass() != Admin.class && currentFirebaseUser != null){
+            if(getArguments() != null) {
+                Bundle args = requireArguments();
+                CookUser user = args.getParcelable("COOK");
+                userName.setText(user.getFirstName() + " " + user.getLastName());
+                userType.setText("Cook Rating: " + user.getRating());
+                userDescription.setText("Cook Description: \n" + user.getDescription());
+                Log.d(TAG, "userType: " + this.signedIn.getUserType());
+            }else {
+                dbRef.child(currentFirebaseUser.getUid()).get().addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DataSnapshot user = task.getResult();
+                        try {
+                            CookUser currentUser = user.getValue(CookUser.class);
+                            if(currentUser!=null) {
+                                if(currentUser.getUserType().equals("client")){
+                                    throw new ClassCastException();
+                                }
+                                userName.setText(currentUser.getFirstName() + " " + currentUser.getLastName());
+                                userType.setText("Cook Rating: " + currentUser.getRating());
+                                userDescription.setText("Cook Description: \n" + currentUser.getDescription());
+                                Log.d(TAG, "userType: " + this.signedIn.getUserType());
+                            }else{
+                                Log.d(TAG, "currentUser is null");
+                            }
+                        } catch (ClassCastException client) {
+                            ClientUser currentUser = user.getValue(ClientUser.class);
+                            if(currentUser != null) {
+                                userName.setText(currentUser.getFirstName() + " " + currentUser.getLastName());
+                                userType.setText("welcome client");
 
-            if(currentUser.getUserType().equals("cook")){
+                                // Centers the welcome text
+                                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                                        LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                                params.gravity = Gravity.CENTER;
+                                userType.setLayoutParams(params);
 
-                CookUser cook = (CookUser) currentUser;
-                userName.setText(currentUser.getFirstName() + " " + currentUser.getLastName());
-                userType.setText("Cook Rating");
-                userDescription.setText(cook.getDescription());
-                Log.d("firebase", "userType: " + this.signedIn.getUserType());
+                                Log.d(TAG, "userType: " + this.signedIn.getUserType());
+                            }else{
+                                Log.d(TAG, "currentUser is null");
+                            }
+                        }
+                    } else {
+                        Log.d(TAG, "Error getting user");
+                    }
+                });
             }
-
-            else if(currentUser.getUserType().equals("client")){
-                userName.setText(currentUser.getFirstName() + " " + currentUser.getLastName());
-                userType.setText("welcome client");
-                Log.d("firebase", "userType: " + this.signedIn.getUserType());
-            }
-
-
         }else{
             userName.setText("Welcome Admin!");
             userType.setText("User Type: " + this.signedIn.getUserType());
-            Log.d("firebase", "userType: " + this.signedIn.getUserType());
+            Log.d(TAG, "userType: " + this.signedIn.getUserType());
         }
 
 //        userType.setText("User Type: " + this.signedIn.getUserType());

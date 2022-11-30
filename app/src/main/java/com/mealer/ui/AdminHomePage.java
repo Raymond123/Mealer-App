d7d272fb0db3ab717247e04d8bef59cc2340f3bb
Easton Smith <eastonsmith37@gmail.com>
2022-11-30 00:42:26 -0500

    commiting minor changes, need to merge with account page before continuing

--- app/src/main/java/com/mealer/ui/AdminHomePage.java
+++ app/src/main/java/com/mealer/ui/AdminHomePage.java
@@ -42,7 +42,7 @@ public class AdminHomePage extends AppCompatActivity implements OnFragmentIntera
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_complaints, R.id.navigation_notifications, R.id.navigation_account)
+                R.id.navigation_complaints, R.id.navigation_account)
                 .build();
 
         final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
